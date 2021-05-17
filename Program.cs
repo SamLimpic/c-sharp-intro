385585407f14056f9454371c9707c064f79206b5
Sam Limpic <samlimpic@gmail.com>
2021-05-17 09:28:20 -0600

    initialized

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// NOTE Equivalent to 'IMPORT" lines
+using System;
+
+namespace c_sharp_intro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // NOTE When C# runs, it looks for this 'MAIN' method first and foremost
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
